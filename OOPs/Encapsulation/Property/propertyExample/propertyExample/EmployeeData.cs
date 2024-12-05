4154a0a23b0904bd074e2d66ecb798ebf44827a5
ysb321 <ysb1571@gmail.com>
2024-12-06 00:09:55 +0530

    OOPs

---
+++ OOPs/Encapsulation/Property/propertyExample/propertyExample/EmployeeData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace propertyExample
+{
+    public class EmployeeDatal
+    {
+        public string EmpName { get; set; } 
+        //It is Property Function where get use for geting data from user and set use for setting the data from user
+        public DateTime EmpJoin { get; set; }
+        public int EmpId { get; set; }
+
+    }
+}
