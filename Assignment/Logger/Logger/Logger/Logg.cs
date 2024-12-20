d98cb80a6fa1a4580b7e93fa26e3a67d6249a549
ysb321 <ysb1571@gmail.com>
2024-12-20 22:28:14 +0530

    Assignment

---
+++ Assignment/Logger/Logger/Logger/Logg.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    public static class Logg
+    {
+        public static string fileType = "\\Log_";
+        public static void Info(string message, string folderName)
+        {
+            if(Directory.Exists(folderName))
+            {
+                DateTime dateTime = DateTime.Now;
+                string formattedDateTime = dateTime.ToString("yyyy-MM-dd_HHMMss");
+                string filePath = Path.Combine(folderName + fileType + formattedDateTime + ".txt");
+                File.WriteAllText(filePath, message);
+                Console.WriteLine("Log File Created");
+            }
+            else
+            {
+                Console.WriteLine("Folder Doesn't Exist");
+            }
+        }
+
+        public static void Debug(string message, string folderName)
+        {
+            fileType = "\\Debug_";
+            Info(message, folderName);
+            Console.WriteLine("This is Debug Message");
+        }
+
+        public static void  Error(string message, string folderName)
+        {
+            fileType = "\\Error_";
+            Info(message, folderName);
+            Console.WriteLine("This is Error Message");
+        }
+    }
+}
---
+++ Assignment/Logger/LoggerProgram/LoggerProgram/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logger;
+
+namespace LoggerProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = "D:\\c and cpp\\C-Sharp\\example";
+            if(!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+
+            Logg.Info("Hello World", folderPath);
+            Logg.Debug("Hello World", folderPath);
+            Logg.Error("Hello World", folderPath);
+        }
+    }
+}
